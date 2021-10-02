1affb0abe023d3080a043c00baef88e6a63faa66
xiuliang.yang <xiuliang.yang@datayes.com>
2021-10-02 18:10:34 +0800

    deploy

--- js/demo1.js
+++ js/demo1.js
@@ -1,16 +1,45 @@
-console.log(lottie);
 
-setTimeout(() => {
+function init() {
     const container = document.createElement('div');
-    // container.innerText = 'loading...';
     document.body.appendChild(container);
 
-    const animation = lottie.loadAnimation({
-        container: container,
-        renderer: 'svg', // 渲染方式:svg、canvas
-        loop: true,  //循环播放，默认：false
-        autoplay: true, //自动播放 ，默认true
-        path: '/blog/json/77440-graph-diagram-animation.json'  // json 路径
-    });
+    const app = new Vue({
+        el: container,
+        template: `
+            
+            <div style="border: 1px solid #80808029">
+                <div style="text-align: center;">
+                    <button @click="play">play</button>
+                    <button @click="stop">stop</button>
+                </div>
+                <div id="container" ></div>
+                
+            </div> 
+      
+        `,
+        methods: {
+            play() {
+                this.animation.play();
+            },
+            stop() {
+                this.animation.stop();
+            }
+        },
+        mounted() {
+            const container = document.getElementById('container');
+            const animation = lottie.loadAnimation({
+                container: container,
+                renderer: 'svg', // 渲染方式:svg、canvas
+                loop: true,  //循环播放，默认：false
+                autoplay: true, //自动播放 ，默认true
+                path: '/blog/json/77440-graph-diagram-animation.json'  // json 路径
+            });
+            this.animation = animation;
+        },
 
-}, 3000);
+    });
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    init();
+}, false);
